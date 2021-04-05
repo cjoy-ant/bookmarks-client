e039647c1475c9e08f4971c49f1985333320b5a3
Christine Antonio <christinejm.antonio@gmail.com>
2021-04-05 14:40:19 -0700

    fix config.js to use window.env

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
-import env  from 'react-dotenv'
-
-const REMOTE_API_URL = env.NODE_ENV === 'production' ? env.REACT_APP_REMOTE_API_URL : process.env.REACT_APP_REMOTE_API_URL || "https://hidden-river-23219.herokuapp.com/api/bookmarks"
-const API_KEY = env.NODE_ENV === 'production' ? env.REACT_APP_API_KEY : process.env.REACT_APP_API_KEY
+const NODE_ENV = process.env.NODE_ENV || "development"
+const REMOTE_API_URL = NODE_ENV === 'production' ? window.env.REACT_APP_REMOTE_API_URL : process.env.REACT_APP_REMOTE_API_URL || "https://hidden-river-23219.herokuapp.com/api/bookmarks"
+const API_KEY = NODE_ENV === 'production' ? window.env.REACT_APP_API_KEY : process.env.REACT_APP_API_KEY
 
 export default {
   REMOTE_API_URL,
