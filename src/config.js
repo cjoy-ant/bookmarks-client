21f31fc80e603282adba322d259c7a04f335315b
Christine Antonio <christinejm.antonio@gmail.com>
2021-04-05 14:56:08 -0700

    update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,4 @@
-const NODE_ENV = process.env.NODE_ENV || "development"
-const REMOTE_API_URL = NODE_ENV === 'production' ? window.env.REACT_APP_REMOTE_API_URL : process.env.REACT_APP_REMOTE_API_URL || "https://hidden-river-23219.herokuapp.com/api/bookmarks"
-const API_KEY = NODE_ENV === 'production' ? window.env.REACT_APP_API_KEY : process.env.REACT_APP_API_KEY
-
 export default {
-  REMOTE_API_URL,
-  API_KEY
+  REMOTE_API_URL: process.env.REACT_APP_REMOTE_API_URL || "https://hidden-river-23219.herokuapp.com/api/bookmarks",
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
