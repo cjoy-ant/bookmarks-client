1224eb3ef9de20684701814afb0a788b280dac7c
Christine Antonio <christinejm.antonio@gmail.com>
2021-04-05 14:32:15 -0700

    update config.js to use dotenv

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
+import env  from 'react-dotenv'
+
+const REMOTE_API_URL = env.NODE_ENV === 'production' ? env.REACT_APP_REMOTE_API_URL : process.env.REACT_APP_REMOTE_API_URL || "https://hidden-river-23219.herokuapp.com/api/bookmarks"
+const API_KEY = env.NODE_ENV === 'production' ? env.REACT_APP_API_KEY : process.env.REACT_APP_API_KEY
+
 export default {
-  REMOTE_API_URL: process.env.REACT_APP_REMOTE_API_URL || "https://hidden-river-23219.herokuapp.com/api/bookmarks",
-  API_KEY: process.env.REACT_APP_API_KEY,
+  REMOTE_API_URL,
+  API_KEY
 }
