f2b19a96e2f4cac81460677d6edc8bad2cb9f62b
Christine Antonio <christinejm.antonio@gmail.com>
2021-04-05 15:25:12 -0700

    updated config to use vercel env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
+const REMOTE_API_URL = process.env.VERCEL_ENV === 'production' ? process.env.REMOTE_API_URL : process.env.REACT_APP_REMOTE_API_URL || "https://hidden-river-23219.herokuapp.com/api/bookmarks"
+const API_KEY = process.env.VERCEL_ENV === 'production' ? process.env.API_KEY : process.env.REACT_APP_API_KEY
+
 export default {
-  REMOTE_API_URL: process.env.REACT_APP_REMOTE_API_URL || "https://hidden-river-23219.herokuapp.com/api/bookmarks",
-  API_KEY: process.env.REACT_APP_API_KEY,
+  REMOTE_API_URL,
+  API_KEY
 }
